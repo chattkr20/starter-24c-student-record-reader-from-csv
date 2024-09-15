b25f8cb9f7075c1ad3fc026d3ee51908457094c7
chattkr20 <chattkr20@gmail.com>
2024-09-14 23:45:10 -0500

    Sub 1

--- src/solution.cc
+++ src/solution.cc
@@ -1,6 +1,45 @@
 #include "solution.hpp"
 
-Student ReadStudentRecFromStream(std::istream& is) {
-  // your implementation here...
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+Student ReadStudentRecFromStream(std::istream& ifs) {
+  if (ifs.good()) {
+    std::string first_name;
+    std::string last_name;
+    unsigned int uin = 0;
+    double gpa = 0.0;
+
+    char throw_away = '\0';
+
+    Student error = Student{};
+
+    ifs >> first_name;
+    if (ifs.fail()) {
+      return error;
+    }
+
+    ifs >> last_name;
+    // if (last_name.at(last_name.size() - 1) != ',' || ifs.fail()) {
+    //   return error;
+    // }
+
+    ifs >> uin >> throw_away;
+    if (throw_away != ',' || ifs.fail()) {
+      return error;
+    }
+
+    ifs >> gpa;
+    if (ifs.fail()) {
+      return error;
+    }
+
+    // std::cout << first_name << " --- " << last_name << " ---- \n";
+
+    return Student{first_name + " " + last_name, uin, gpa};
+  }
+
   return Student{};
 }
